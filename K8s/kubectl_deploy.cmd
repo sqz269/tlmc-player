32a3893eb13877afa1c1d6360666ceaceb770c27
sqz269 <yin2690855381@gmail.com>
2022-12-26 16:29:44 -0500

    updated deployment script

--- K8s/kubectl_deploy.cmd
+++ K8s/kubectl_deploy.cmd
@@ -1,3 +1,10 @@
+kubectl.exe apply -f .\auth-pgsql-pv-pvc.yaml
+kubectl.exe apply -f .\auth-pgsql-depl.yaml
+kubectl.exe apply -f .\auth-pgsql-svc.yaml
+
+kubectl.exe apply -f .\auth-api-depl.yaml
+kubectl.exe apply -f .\auth-api-svc.yaml
+
 kubectl.exe apply -f .\musicdata-pgsql-pv-pvc.yaml
 kubectl.exe apply -f .\musicdata-pgsql-depl.yaml
 kubectl.exe apply -f .\musicdata-pgsql-svc.yaml
