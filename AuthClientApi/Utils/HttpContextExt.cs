56b55317af2cbccd82541017c8e8fcbef568443a
sqz269 <yin2690855381@gmail.com>
2023-01-16 21:33:13 -0500

    Updated HttpContext get key

--- AuthClientApi/Utils/HttpContextExt.cs
+++ AuthClientApi/Utils/HttpContextExt.cs
@@ -5,7 +5,7 @@ public static class HttpContextExt
     public static UserClaim? GetUserClaim(this HttpContext? context)
     {
         if (context == null) return null;
-        if (context.Items.TryGetValue("CustUser", out var item))
+        if (context.Items.TryGetValue("UserClaim", out var item))
         {
             return (UserClaim?)item;
         }
