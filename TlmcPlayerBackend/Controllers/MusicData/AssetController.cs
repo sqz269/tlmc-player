5398885bc187f5995698857da5b75d7f8c88e2e8
sqz269 <yin2690855381@gmail.com>
2024-12-22 11:57:53 -0500

    Make asset API default serve non-download

--- TlmcPlayerBackend/Controllers/MusicData/AssetController.cs
+++ TlmcPlayerBackend/Controllers/MusicData/AssetController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using FFMpegCore.Arguments;
+using Microsoft.AspNetCore.Mvc;
 using TlmcPlayerBackend.Data.Api.MusicData;
 
 namespace TlmcPlayerBackend.Controllers.MusicData;
@@ -7,6 +9,26 @@ namespace TlmcPlayerBackend.Controllers.MusicData;
 [Route("api/asset")]
 public class AssetController : Controller
 {
+    private static readonly HashSet<string> PREVIEWABLE_MIME_TYPES = [
+        "image/jpeg",
+        "image/png",
+        "image/gif",
+        "image/bmp",
+        "image/webp",
+        "image/x-icon",
+        "text/plain",
+        "text/html",
+        "text/xml",
+        "video/mp4",
+        "video/webm",
+        "video/mpeg",
+        "audio/mpeg3",
+        "audio/ogg",
+        "audio/mp4",
+        "audio/flac",
+        "audio/midi",
+    ];
+
     private readonly IAssetRepo _assetRepo;
 
     public AssetController(IAssetRepo assetRepo)
@@ -15,16 +37,21 @@ public class AssetController : Controller
     }
 
     [HttpGet("{id:Guid}", Name = nameof(GetAsset))]
-    public async Task<IActionResult> GetAsset(Guid id)
+    public async Task<IActionResult> GetAsset(Guid id, [FromQuery] bool download = false)
     {
         var asset = await _assetRepo.GetAssetById(id);
         if (asset == null)
             return NotFound();
 
-        FileStream fileStream = new FileStream(asset.Path, FileMode.Open, FileAccess.Read, FileShare.Read);
+        var fileStream = new FileStream(asset.Path, FileMode.Open, FileAccess.Read, FileShare.Read);
 
-        string mime = asset.Mime ?? "application/octet-stream";
+        var mime = asset.Mime ?? "application/octet-stream";
 
-        return File(fileStream, mime, asset.Name, enableRangeProcessing: asset.Size > 1000000);
+        if (download)
+        {
+            return File(fileStream, mime, asset.Name, enableRangeProcessing: asset.Size > 1000000);
+        }
+
+        return File(fileStream, mime);
     }
 }
